9c5b37d0940d855cf0ae0047049e651ecedd4d8e
Jakub Więckowski <126171977+Wiecus@users.noreply.github.com>
2024-11-04 17:43:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_wikipedia_search_results(query):
-    search_url = f"https://pl.wikipedia.org/w/api.php?action=query&list=search&srsearch={query}&format=json"
-    response = requests.get(search_url)
-    return response.json()
 
-def extract_wikipedia_info(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+def fetch_article_data(article_url):
+    response = requests.get(article_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, 'html.parser')
 
     internal_links = []
-    for a in soup.select('a[href^="/wiki/"]:not([href*=":"])'):
-        if len(internal_links) < 5:
-            link_text = a.text.strip()
-            link_url = 'https://pl.wikipedia.org' + a['href']
-            if link_text and link_url not in [link[1] for link in internal_links]:
-                internal_links.append((link_text, link_url))
+    for a in soup.select('a[href^="/wiki/"]'):
+        if len(internal_links) < 5 and "Kategoria:" not in a['href']:
+            internal_links.append(f"{a.text} | https://pl.wikipedia.org{a['href']}")
 
-    image_urls = []
+    images = []
     for img in soup.select('img'):
-        if len(image_urls) < 3:
-            img_src = img['src']
-            if not img_src.startswith('http'):
-                img_src = 'https:' + img_src
-            image_urls.append(img_src)
+        if len(images) < 3:
+            images.append(f"https://upload.wikimedia.org{img['src']}")
 
     external_links = []
-    for a in soup.select('a[href^="http"]:not([href*="wikipedia.org"])'):
+    for a in soup.select('a.external'):
         if len(external_links) < 3:
             external_links.append(a['href'])
 
     categories = []
-    for a in soup.select('div#mw-normal-catlinks ul li a'):
+    for a in soup.select('a[title^="Kategoria:"]'):
         if len(categories) < 3:
-            categories.append(a.text.strip())
+            categories.append(a.text)
 
-    return internal_links, image_urls, external_links, categories
+    return {
+        'internal_links': internal_links,
+        'images': images,
+        'external_links': external_links,
+        'categories': categories
+    }
+
 
 def main():
-    category = input("Podaj kategorię (np. Miasta na prawach powiatu): ")
-    search_results = get_wikipedia_search_results(category)
-    articles = [f"https://pl.wikipedia.org/wiki/{result['title'].replace(' ', '_')}" for result in search_results['query']['search'][:2]]
+    search_query = input("Podaj kategorię (np. 'Miasta na prawach powiatu'): ")
+    category_url = f"https://pl.wikipedia.org/wiki/Kategoria:{requests.utils.quote(search_query)}"
 
-    if not articles:
-        print("No articles found.")
+    response = requests.get(category_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    article_links = soup.select('.mw-category-group a')[:2]
+
+    if not article_links:
+        print("Nie znaleziono artykułów w tej kategorii.")
         return
 
-    for article_url in articles:
-        internal_links, image_urls, external_links, categories = extract_wikipedia_info(article_url)
-        print(f"Article URL: {article_url}")
-        print("Internal Links:")
-        internal_links_text = " | ".join(f"{link_text} | {link_url}" for link_text, link_url in internal_links)
-        print(internal_links_text if internal_links_text else "")
-        print("Image URLs:")
-        image_urls_text = " | ".join(image_urls)
-        print(image_urls_text if image_urls_text else "")
-        print("External Links:")
-        external_links_text = " | ".join(external_links)
-        print(external_links_text if external_links_text else "")
-        print("Categories:")
-        categories_text = " | ".join(categories)
-        print(categories_text if categories_text else "")
-        print("\n")
+    for article_link in article_links:
+        article_url = f"https://pl.wikipedia.org{article_link['href']}"
+        article_data = fetch_article_data(article_url)
 
-if __name__ == '__main__':
+        output_lines = []
+        output_lines.append(f"Article URL: {article_url}")
+        output_lines.append(
+            "Internal Links:\n" + "\n".join(article_data['internal_links']) if article_data['internal_links'] else "")
+        output_lines.append("Image URLs:\n" + "\n".join(article_data['images']) if article_data['images'] else "")
+        output_lines.append(
+            "External Links:\n" + "\n".join(article_data['external_links']) if article_data['external_links'] else "")
+        output_lines.append(
+            "Categories:\n" + "\n".join(article_data['categories']) if article_data['categories'] else "")
+
+        print("\n".join(output_lines))
+        print("\n" + "-" * 40 + "\n")  # Separator between articles
+
+
+if __name__ == "__main__":
     main()
